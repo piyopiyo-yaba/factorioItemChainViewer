7f4c31211951c01f2dd47c7331e80ef80d1f7bf9
piyopiyoSH <53153133+piyopiyoSHOHEI@users.noreply.github.com>
2020-01-19 02:37:20 +0900

    プロセス名の考慮と設備

--- js/FactorioItems_0.js
+++ js/FactorioItems_0.js
@@ -4,28 +4,97 @@ FactorioItems = function (configs) {
 
 };
 
-FactorioItems.prototype.initItems = function (configs) {
+FactorioItems.prototype.initItems = function (configs) {	// アイテムインスタンスをプロパティに登録する
 
 	configs.forEach( (config) => {
 
-		const {name, recipe} = config;
 
-		if (! name) return;
-		if (typeof this[name] !== "undefined") {
-			return;
+		let 	itemType;
+		const {name, processName, recipe, facility} = config;
+
+// --------------------------------------------------FromAssemblingMachine (n)--------------------------------
+
+		if ( facility === "assembling-machine-1") itemType = "FromAssemblingMachine";
+
+// --------------------------------------------------FromAssemblingMachineTakingFluid (n)---------------------
+
+		else if ( facility === "assembling-machine-2") itemType = "FromAssemblingMachineTakingFluid";
+
+// --------------------------------------------------FromRefinery (n)-----------------------------------------
+
+		else if ( facility === "oil-refinery") itemType = "FromRefinery";
+
+// --------------------------------------------------FromChemicalPlant (n)------------------------------------
+
+		else if ( facility === "chemical-plant") itemType = "FromChemicalPlant";
+
+// --------------------------------------------------FromFurnace (n)------------------------------------------
+
+		else if ( facility === "stone-furnace") itemType = "FromFurnace";
+
+// --------------------------------------------------FromMining (n)-------------------------------------------
+
+		else if ( name.endsWith("ore") || name === "coal" || name === "stone") itemType = "FromMining";
+
+// --------------------------------------------------FromCentrifuge (n)---------------------------------------
+
+		else if ( facility === "centrifuge") itemType = "FromCentrifuge";
+
+// --------------------------------------------------FromHand (1)---------------------------------------------
+
+		else if ( name === "fish" || name === "raw-wood") itemType = "FromHand";
+
+// --------------------------------------------------FromPump (1)---------------------------------------------
+
+		else if ( name === "water") itemType = "FromPump";
+
+// --------------------------------------------------FromBoiler (1)-------------------------------------------
+
+		else if ( name === "steam") itemType = "FromBoiler";
+
+// --------------------------------------------------FromPumpjack (1)-----------------------------------------
+
+		else if ( name === "crude-oil") itemType = "FromPumpjack";
+
+// --------------------------------------------------FromRocketSilo (1)---------------------------------------
+
+		else if ( facility === "rocket-silo") itemType = "FromRocketSilo";
+
+// --------------------------------------------------crude-oil-barrelは無視-----------------------------------
+
+		else if ( name === "crude-oil-barrel") return;
+
+// --------------------------------------------------call factory---------------------------------------
+
+
+		if (typeof itemType === "undefined") {
+			throw {
+				name: "Error",
+				message: `${name} does not exist`,
+			};
 		}
 
-		this[name] = new FactorioItem(config);
+
+
+		if (processName) {
+
+			this[`${name}_(${processName})`] = new FactorioItem.factory(itemType, config);
+
+		} else {
+
+			this[name] = new FactorioItem.factory(itemType, config);
+
+		}
 
 	});
 
 };
 
-FactorioItems.prototype.createTree = function (userInput) {
+FactorioItems.prototype.createTree = function (userInput) {			// 明日はここから直そうね。productName_(productProcess)は....
 
-	const	{inputProductNum, inputProductName} = userInput,
-		partsName = [],
-		partsNum = [],
+	const	{inputProductNum, inputProductName} = userInput,		// ユーザーが選んだアイテム名と、その数
+		partsName = [],				 
+		partsNum = [],														
 		partsEdge  = [];
 
 	let	item = this[inputProductName],
@@ -38,10 +107,12 @@ FactorioItems.prototype.createTree = function (userInput) {
 	partsName.push(inputProductName);		
 	partsNum.push(inputProductNum);
 	
-	while(typeof partsName[index] !== "undefined") {
+	while(typeof partsName[index] !== "undefined") {			// アイテムのレシピを基に木構造を作っていく
 
 		productName = partsName[index];
 		productNum = partsNum[index];
+
+		if (typeof (item = this[productName]) === "undefined") 
 
 		item = this[productName];
 		item.accProductNum(productNum);
